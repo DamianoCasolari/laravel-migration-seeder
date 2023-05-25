b8b53f5581963802b1118629dfa7aab50972d9c0
DamianoCasolari <damianocasolari@gmail.com>
2023-05-25 17:46:08 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header class="p-4 text-light bg-dark">
+    <h1 class="text-center">Arrivals departures monitor</h1>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,109 @@
+@extends('layout.app')
+@section('page-title, Home Page')
+@section('content')
+    <div class="my_container d-flex flex-column justify-content-center align-items-center">
+
+        <table>
+            <tr class="p-3 text-center">
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+                <th class="p-3">Top </th>
+            </tr>
+            <tr class="p-3 text-center ">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+            <tr class="p-3 text-center">
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+                <td class="p-3">info </td>
+            </tr>
+
+        </table>
+    </div>
+@endsection
